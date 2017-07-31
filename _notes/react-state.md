029f8e72e075a2ec149f0f039ac568163a3c5b86
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-07-31 15:21:57 -0700

    update

---
+++ _notes/react-state.md
@@ -0,0 +1,4 @@
+To make components that respond to
+user input and other events, React uses a data structure called state in the component.
+The state essentially holds the model, something that can change, as opposed to the
+immutable properties in the form of props that you saw earlier
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
-const contentNode = document.getElementById('contents');
+const issues = [
+    {
+        id: 1, status: 'Open', owner: 'Ravan',
+        created: new Date('2016-08-15'), effort: 5, completionDate: undefined,
+        title: 'Error in console when clicking Add',
+    },
+    {
+        id: 2, status: 'Assigned', owner: 'Eddie',
+        created: new Date('2016-08-16'), effort: 14,
+        completionDate: new Date('2016-08-30'),
+        title: 'Missing bottom border on panel',
+    },
+];
+
 class IssueFilter extends React.Component {
     render() {
         return (
@@ -67,4 +80,5 @@ class IssueRow extends React.Component {
     }
 }
 
+const contentNode = document.getElementById('contents');
 ReactDOM.render(<IssueList />, contentNode);
